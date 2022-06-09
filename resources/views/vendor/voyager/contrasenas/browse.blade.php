579a7e9d42f98b217e917accd174c7747bbccd0c
NoheliGT <87887078+NoheliGT@users.noreply.github.com>
2022-06-09 11:55:27 -0500

    Contraseñas

---
+++ resources/views/vendor/voyager/contrasenas/browse.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+                    .button {
+            background-color: #2ecc71;
+            border-radius: 6px;
+            border: none;
+            color: #fff;
+            cursor: pointer;
+            font-size: 16px;
+            padding: 10px 20px;
+            text-align: center;
+            width: auto;
+            height:;
+            float: right;
+            }
+            .button:hover {
+            background-color: #FB7E29;
+            color: #fff;
+            }
+    </style>
+</head>
+<body>
+    <p>hola</p>
+    <a class="button" href="{{ route('voyager.alumnos.index') }}">← Regresar</a>
+
+</body>
+</html>
